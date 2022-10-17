ad799b6bc3bf5fc4006215bc333e58e6a435a406
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2022-10-16 23:40:30 -0400

    Global Round 23: C: post-contest: improve comment

--- Live/global_round_23/src/bin/c_permutation_operations_simple.rs
+++ Live/global_round_23/src/bin/c_permutation_operations_simple.rs
@@ -21,9 +21,10 @@ fn main() {
 
         // In operation i, choose the index of that a_i which has value n + 1 - i.
         // If i is in the set {1,...,n} then so is n + 1 - i, so there is such a unique a_i.
-        // If we only added i to the value at this index, then all the values would be n + 1.
+        // If we were only adding i to the value at this index,
+        // then all the values would become n + 1 after the n operations.
         // Even this would be sufficient to eliminate all inversions.
-        // By also adding this same value to all subsequent indices as well,
+        // By also adding i to the values at all subsequent indices as well
         // we don't change their inversion statuses relative to one another,
         // but we do make them even larger than numbers at earlier indices.
 
